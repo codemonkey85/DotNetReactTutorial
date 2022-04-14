b7f3d42ed5d0e03d2bce1ae76b86854e59101a88
Michael Bond <codemonkey8510@icloud.com>
2022-04-14 00:54:31 -0400

    Some cleanup and stuff

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
+// import { Header, List } from "semantic-ui-react";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://localhost:7147/api/activities")
+      .then((response) => setActivities(response.data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello world</h1>
+        <ul>
+          {activities.map((activity: any) => (
+            <li key={activity.id}>{activity.title}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
