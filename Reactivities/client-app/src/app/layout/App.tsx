841bb0d2f346169cbfea0736268df2c58ab43870
Michael Bond <codemonkey8510@icloud.com>
2022-04-14 11:51:30 -0400

    Remove unused imports

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,11 +1,9 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import axios from "axios";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { Col, Container } from "react-bootstrap";
-import ActivityDetails from "../../features/activities/details/ActivityDetails";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
