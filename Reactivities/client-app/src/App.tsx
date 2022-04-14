ff8535c5384c6f0cfdbe016c2c915b0759c6f045
Michael Bond <codemonkey8510@icloud.com>
2022-04-14 00:56:41 -0400

    Remove unused code

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
-// import { Header, List } from "semantic-ui-react";
 
 function App() {
   const [activities, setActivities] = useState([]);
